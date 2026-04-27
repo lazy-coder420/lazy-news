ddbee6bdcd84265301a2d91dc7df8abd1c6c8cba
lazy-coder420 <mjmunna692@gmail.com>
2026-04-27 21:51:34 +0600

    atuh setup dan and cantion dan

---
+++ src/lib/auth.js
@@ -0,0 +1,26 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+const client = new MongoClient(process.env.DB_URI);  //mongodb account 
+const db = client.db();
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db, {
+    // Optional: if you don't provide a client, database transactions won't be enabled.
+    client
+  }),
+});
+
+
+
+
+
+
+
+
+
+faQRabDgAO7xZQYr
+//    passs
+
+lazy-news
