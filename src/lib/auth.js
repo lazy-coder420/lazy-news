6a5c4ac4869b21420a8b502da01489d1abefb075
lazy-coder420 <mjmunna692@gmail.com>
2026-04-28 02:00:00 +0600

    now sueccssful work gitgub authcation

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,6 +3,8 @@ import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.DB_URI);  //mongodb account 
+
+await client.connect();
 const db = client.db("munna");
 
 export const auth = betterAuth({
@@ -21,6 +23,14 @@ export const auth = betterAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         },
     },
+
+
+    socialProviders: {
+        github: {
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        },
+    }
 
 
 });
@@ -51,3 +61,6 @@ export const auth = betterAuth({
 
 
 // github
+// Ov23lizzv6H6bf0UH2vH
+
+// 64b4542ba0d89c37c5208d9fbc5d80239fbe93cf
