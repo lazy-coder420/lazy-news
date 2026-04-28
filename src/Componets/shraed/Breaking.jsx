5ed0c3285fb5a6894af89c1845bd86396feee5e2
lazy-coder420 <mjmunna692@gmail.com>
2026-04-28 12:14:51 +0600

    news part diy

--- src/Componets/shraed/Breaking.jsx
+++ src/Componets/shraed/Breaking.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
 import Marquee from 'react-fast-marquee'
 
+
+const news = [
+    {
+        _id: "1",
+        title: "Breaking News: Major Event Unfolds in the City",
+    },
+    {
+        _id: "2",
+        title: "Breaking News: New Policy Announced by the Government",
+    },
+    {
+        _id: "3",
+        title: "Breaking News: Sports Team Wins Championship",
+    },
+];
+
+
+
 const Breaking = () => {
     return (
-        <div className='flex justify-between gap-4 items-center bg-gray-300 container mx-auto px-4 py-2'>
-            <button className='btn btn-error text-white '>Latest</button>
-            <Marquee pauseOnHover={true} speed={100} >
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla assumenda minima doloremque. Voluptate laborum quo in enim nam. Exercitationem odit cumque temporibus eos id ipsam minima molestiae ad perferendis quae?
+        <div className="flex justify-between gap-4 items-center bg-gray-200 py-4 px-2 container mx-auto">
+            <button className="btn bg-red-500 text-white">Latest News</button>
+            <Marquee pauseOnHover={true} speed={100}>
+                {news.map((n) => (
+                    <span key={n._id}>{n.title}</span>
+                ))}
             </Marquee>
         </div>
     )
